f3e0e7a6f9e1633afbb3972953965d056ce5af70
shnaboi <shane.karner@gmail.com>
2023-03-25 16:16:56 -0400

    Setting up HTML, slight css

---
+++ app.js
@@ -0,0 +1,29 @@
+const gameBoard = document.querySelector('.game-board')
+
+let gameArray = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Describe common bugs you might run into using constructors.
+
+// Write a factory method that returns an object.
+// Explain how scope works in JavaScript (bonus points if you can point out what ES6 changed!).
+// Explain what Closure is and how it impacts private functions & variables.
+// Describe how private functions & variables are useful.
+// Use inheritance in objects using the factory pattern.
+// Explain the module pattern.
+// Describe IIFE. What does it stand for?
+// Briefly explain namespacing and how it’s useful.
